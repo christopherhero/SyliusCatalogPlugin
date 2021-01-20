fd12e7af2ab2b56d4f5ed79f2fb15b432c11bbe7
Mateusz Deszert-Klosowski <mateusz.deszert-klosowski@bitbag.pl>
2021-01-20 08:24:52 +0100

    Typo

--- src/Resources/public/js/bitbag-catalog-rules.js
+++ src/Resources/public/js/bitbag-catalog-rules.js
@@ -1,6 +1,6 @@
 (function($) {
     $(document).ready(function () {
-        $('.bitbag-rules#product_association_rule a[data-form-collection="add"]').on('click', (event) => {
+        $('.bitbag-rules#product_association_rules a[data-form-collection="add"]').on('click', (event) => {
             const name = $(event.target).closest('form').attr('name');
             setTimeout(() => {
                 $(`select[name^="${name}[productAssociationRules]"][name$="[type]"]`).last().change();
